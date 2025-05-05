462a02163dd27a3f000b9821aaa0feb5ee1f416f
Shady <mshady9003@gmail.com>
2025-05-05 02:44:38 +0200

    first endpoint

--- src/api_json_export.py
+++ src/api_json_export.py
@@ -14,7 +14,10 @@ AAS_JSON_PATH = Path(r"C:\Users\dell\PycharmProjects\bam-aas-timeseries\data\Tim
 # Define the path to your JSON file
 AAS_JSON_PATH = Path(r"C:\Users\dell\PycharmProjects\bam-aas-timeseries\data\TimeSeriesAAS.json")
 
-@app.get("/api/v1/aas/{asset_id}/submodels/time-series")
+
+@app.get("/api/v1/aas/{asset_id}/submodels/time-series",
+         summary="Get Time Series Submodel",
+         description="Returns the complete AAS submodel in JSON format")
 def get_submodel(asset_id: str):
     if not AAS_JSON_PATH.exists():
         raise HTTPException(status_code=404, detail="AAS Submodel JSON file not found.")
